15f4279d084170f3d3a04eb812acb7abffb6cefa
Alexandra Goldobina <goldobina.s@gmail.com>
2025-06-02 18:26:34 +0500

    Добавлена админ панель для моделей Task и Comment

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task, Comment
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели "Task" в административной панели."""
+
+    list_display = (
+        "pk",
+        "title",
+        "status",
+        "deadline",
+        "task_performer",
+    )
+
+    list_filter = (
+        "status",
+        "deadline",
+        "task_performer",
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели "Comment" в административной панели."""
+    list_display = (
+        "pk",
+        "text",
+        "author",
+        "task",
+    )
